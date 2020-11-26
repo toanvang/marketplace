55f3bef843d6c5ccd91d50d86c3a6345e271f16e
Jiong Wu <lohengrin1024@gmail.com>
2020-11-25 19:23:26 -0800

    change port

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
 require('./controllers/quiz-attempts.controller.server')(app)
 
-app.listen(3000);
+app.listen(process.env.PORT || 8080);

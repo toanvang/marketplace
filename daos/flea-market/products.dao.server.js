e8db8f5e076aa273afba8dce30b794b0661b62bd
Jiong Wu <lohengrin1024@gmail.com>
2020-11-28 20:25:02 -0800

    populate owner to username and _id

--- daos/flea-market/products.dao.server.js
+++ daos/flea-market/products.dao.server.js
@@ -1,8 +1,13 @@
 const productModel = require("../../models/flea-market/products/products.model.server")
 
 const createProduct = (newProduct) => productModel.create(newProduct)
-const findAllProducts = () => productModel.find()
-const searchProducts = (searchConditions) => productModel.find(searchConditions)
+
+const findAllProducts = () =>
+  productModel.find().populate('owner', 'username _id')
+
+const searchProducts = (searchConditions) =>
+  productModel.find(searchConditions).populate('owner', 'username _id')
+
 const updateProduct = (pid, product) => productModel.findByIdAndUpdate({pid}, product)
 const deleteProduct = (pid) => productModel.deleteOne({_id: pid})
 
